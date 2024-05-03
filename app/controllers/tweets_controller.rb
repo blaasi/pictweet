9f3ae9bfdbba6b9b83b0a8e7d68a7c32f31b3256
blaasi <katsuyuki2444@gmail.com>
2024-05-03 14:07:20 +0900

    削除機能（destroyアクション）の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,13 @@ class TweetsController < ApplicationController
 
   def create
     Tweet.create(tweet_params)
-    redirect_to '/'
+    redirect_to root_path
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
   end
 
   private
