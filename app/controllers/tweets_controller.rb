78bc1a4eab8eb376343f57ed0632457af4cd178b
blaasi <katsuyuki2444@gmail.com>
2024-05-04 10:49:40 +0900

    編集機能(editアクション,updateアクション)の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+
   def index
     @tweets = Tweet.all
   end
@@ -18,7 +19,18 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
   private
+
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end
