0593f5b22848bb4903ccb3408a8733b659f7c628
blaasi <katsuyuki2444@gmail.com>
2024-05-05 20:14:06 +0900

    アソシエーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_05_102503) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_05_110748) do
   create_table "tweets", charset: "utf8", force: :cascade do |t|
     t.string "name"
     t.string "text"
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
