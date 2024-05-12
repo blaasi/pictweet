a669127e709da22f021dd06b2d64307343afa9c5
blaasi <katsuyuki2444@gmail.com>
2024-05-12 15:08:48 +0900

    commentモデルの作成、ルーティングのネスト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_05_113340) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_12_053121) do
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", charset: "utf8", force: :cascade do |t|
     t.string "text"
     t.string "image"
